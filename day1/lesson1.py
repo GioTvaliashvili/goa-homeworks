72057f17e077a20430407a46a7e32171952996d6
GioTvaliashvili <babucidze86@gmail.com>
2024-02-16 22:30:28 +0400

    commit

---
+++ day1/lesson1.py
@@ -0,0 +1,7 @@
+name="gio"
+surname="tvaliashvili"
+age=11
+height=148.5
+knows_programming=False
+is_ugly=False
+print ("my name is" + " " +name + " "+ surname + " i am " + str (age)+" Years old " + " and i am " + str(height)+ " in height" )
