e9254f7a0fcfba7d8668f4df8353e91ba648f97b
GioTvaliashvili <159298551+GioTvaliashvili@users.noreply.github.com>
2024-02-25 00:56:06 +0400

    Add files via upload

---
+++ day4/lesson4.py
@@ -0,0 +1,74 @@
+print( 5 + 7)
+print(8 + 10)
+print(100 + 343)
+print(83 + 67)
+print(892 + 783)
+print(674 + 824)
+print(231 + 139)
+print(8 + 6)
+print(362 + 786)
+print(100 + 400)
+
+
+
+
+
+
+
+
+
+
+
+print(4 - 1)
+print(6 - 3)
+print(5 - 2)
+print(72 - 38)
+print(10 - 10)
+print(56 - 34)
+print(128 - 79)
+print(54 - 32)
+print(566 - 147)
+print(50 - 20)
+
+
+
+
+
+
+
+
+
+
+
+
+print(1 * 2)
+print(56 * 5)
+print(166 * 127)
+print(500 * 500)
+print(1 * 1)
+print(2 * 8)
+print(100 * 8)
+print(50 *168)
+print(92 * 193)
+print(381 *926)
+
+
+
+
+
+
+
+
+
+
+print( 128 / 4)
+print(900 / 3)
+print(200 / 2)
+print(64 /4)
+print(256 / 2)
+print(100 / 5)
+print(250 / 5)
+print(2 / 2)
+print(8 / 4)
+print(6 / 3)
+
